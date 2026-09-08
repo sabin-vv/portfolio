003b1b13f30e9b2bcd4309cf6097a9d2f3c4af8a
sabin-vv <vvsabin@gmail.com>
2026-09-08 18:37:41 +0530

    Update FeaturedWork to use featuredProjects array

--- src/components/FeaturedWork.tsx
+++ src/components/FeaturedWork.tsx
@@ -1,16 +1,16 @@
 import SectionHeader from '@/components/SectionHeader'
 import ProjectCard from '@/components/ProjectCard'
-import { projects } from '@/data/projects'
+import { featuredProjects } from '@/data/projects'
 
 export default function FeaturedWork() {
-    const featured = projects.find((project) => project.featured)
-
-    if (!featured) return null
+    if (!featuredProjects.length) return null
 
     return (
-        <section className="flex flex-col gap-8">
+        <section id="work" className="flex scroll-mt-24 flex-col gap-8">
             <SectionHeader title="Featured Work" />
-            <ProjectCard project={featured} />
+            {featuredProjects.map((project) => (
+                <ProjectCard key={project.id} project={project} />
+            ))}
         </section>
     )
 }
